7b42d19fa3970918674e9e76170da7251f53b511
Apple <apple@Apples-MacBook-Pro-13.local>
2019-08-08 13:23:19 -0700

    we started something pt.2

--- Emojional App/Emojional App/ViewController.swift
+++ Emojional App/Emojional App/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "🥳", message: "Who gave you permission to be so cute?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
